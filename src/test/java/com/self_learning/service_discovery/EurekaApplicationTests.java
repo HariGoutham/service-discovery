a56ff678f83b0c831c3d3000ce118c1de1f6b9b4
HariGoutham <goutham540@gmail.com>
2025-02-07 00:18:24 +0800

    Eureka server initial local setup done

---
+++ src/test/java/com/self_learning/service_discovery/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self_learning.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
